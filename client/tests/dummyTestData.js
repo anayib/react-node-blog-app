3de3d5cfae87aea23c6b910faa08bb13b7915fc5
Nayib <nayib.abdala@gmail.com>
2020-06-26 14:30:30 -0500

    Update tests Context text pending for revision

---
+++ client/tests/dummyTestData.js
@@ -0,0 +1,16 @@
+const dummyTestData = [
+  {
+    id: "introduccion-a-node-js",
+    title: "Introducción a Node JS",
+    tags: ["Node", "JavaScript"],
+    metaDescription: "Node, aprender node JS para directores de producto",
+  },
+  {
+    id: "introduccion-a-react-js",
+    title: "Introducción a React JS",
+    tags: ["React", "JavaScript"],
+    metaDescription: "React, aprender react para directores de producto",
+  },
+];
+
+export default dummyTestData;
---
+++ client/tests/showTutorials.test.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { ContextProvider } from "../Context";
+// import Adapter from "enzyme-adapter-react-16";
+// import { mount, configure } from "enzyme";
+// import Tutorials from "../pages/Tutorials";
+
+// configure({ adapter: new Adapter() });
+
+// describe("<Tutorials />", () => {
+//   it("Should render two articles", () => {
+//     const wrapper = mount(
+//       <ContextProvider>
+//         <Tutorials />
+//       </ContextProvider>
+//     );
+//     console.log(wrapper.debug({ verbose: true }));
+//     expect(wrapper.find(".show-content").exists()).toEqual(true);
+//   });
+// });
